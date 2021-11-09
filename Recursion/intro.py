9618568e02c53b3d0d39c0a78a82810cbced76e2
Mouizudiin <mouizuddin26@gmail.com>
2021-11-09 11:28:19 +0530

    Recursion

---
+++ Recursion/intro.py
@@ -0,0 +1,22 @@
+'''Recursion
+1) Recursive case
+2) Base Case (to stop)
+3) Un-intentional Case -  the constrain
+
+'''
+def recursion_method(num):
+    if num  <=0:
+        return False
+    return f'{num} _{recursion_method(num-1)}'
+
+# print(recursion_method(10))
+
+# Factorial -> n! = n * (n-1)!
+
+def factorial(num):
+    assert num >= 0 and type(num) == int,"number should be positive and integers only"
+    if num in [0,1]:
+        return  1
+    return num * factorial(num-1)
+
+print(factorial(3))
