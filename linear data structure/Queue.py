4400228c6650277bbfb0cad647ed0c5f1dfeef95
Mouizudiin <mouizuddin26@gmail.com>
2022-01-27 11:43:15 +0530

    Queue

--- linear data structure/Queue.py
+++ linear data structure/Queue.py
@@ -6,7 +6,6 @@
 5) One end is always used to insert data (enqueue) and the other is used to remove data (dequeue).
 6) Insertion and deletion happnes on differenet end's
 
-
 '''
 print('=========== Queue implementation using list ===========\n')
 queue = []
@@ -46,48 +45,49 @@ def selction_sort():
 def queue_implementation():
 
     while True:
-        choose = int(input('1 - to add , '
-                           '2 - to remove , '
-                           '3 - for the top element , '
-                           '4 - for base element'
-                           '5 - to sort the Queue'
-                           '0 - to quit'))
-        ''''
-        if 1,2,3,4,0  100 > else False
-        print(__docs__)
-        '''
-        # if choose !>=4:
+        try:
+            choose = int(input('1 - to add\n '
+                               '2 - to remove\n '
+                               '3 - for the top element\n'
+                               '4 - for base element\n'
+                               '5 - to sort(selection sort) the Queue\n'
+                               '0 - to quit\n'))
 
-        if choose == 1:
-            to_add()
-        elif choose == 2:
-            to_remove()
-        elif choose == 3:
-            try:
-                if not queue:
-                    print(f'No element queue is empty{queue}')
-                top_element()
-            except IndexError as index:
-                print(f'Error is {index}')
+            # if choose !>=4:
+            if choose >5 :
+                print('in range only (0-5)')
+            else:
+                if choose == 1:
+                    to_add()
+                elif choose == 2:
+                    to_remove()
+                elif choose == 3:
+                    try:
+                        if not queue:
+                            print(f'No element queue is empty{queue}')
+                        top_element()
+                    except IndexError as index:
+                        print(f'Error is {index}')
 
-        elif choose == 4:
-            try:
-                if not queue:
-                    print(f'No element queue is empty{queue}')
-                if len(queue) >=2 :
-                    base_element()
-                else:
-                    print(None,end=' ')
-                    print(f'{queue} should have atleast 2 elements to print base')
-            except IndexError as index:
-                print(f'Error is {index}')
-        elif choose == 5:
-            selction_sort()
-        elif choose == 0:
-            print(f'Final queue is {queue}')
-            break
+                elif choose == 4:
+                    try:
+                        if not queue:
+                            print(f'No element queue is empty{queue}')
+                        if len(queue) >= 2:
+                            base_element()
+                        else:
+                            print(None, end=' ')
+                            print(f'{queue} should have atleast 2 elements to print base')
+                    except IndexError as index:
+                        print(f'Error is {index}')
+                elif choose == 5:
+                    selction_sort()
+                elif choose == 0:
+                    print(f'Final queue is {queue}')
+                    break
+        except ValueError as value:
+            print('Only interegrs')
+            print(value)
 
-
-queue_implementation()
-
-# not handled ValueError in this implementation
+if __name__ == '__main__':
+    queue_implementation()
